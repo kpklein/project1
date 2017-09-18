e08e983cbacaf7e3140524970f253a2050566bc4
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2017-09-18 15:43:06 +0100

    fixed button issue

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var player = 1;
 
 	$('.box').on('click', function(event){
-	
+
 
 		var boxSelected = $(this);
 
@@ -33,34 +33,35 @@ $(document).ready(function(){
 	}
 
 	});
+
+
+function checkIfPlayerWon(symbol){
+		// x
+		// o
+
+	if($('.bx1').hasClass(symbol) && $('.bx2').hasClass(symbol) && $('.bx3').hasClass(symbol) && $('.bx4').hasClass(symbol)) {
+	return true;
+	} else if ($('.bx1').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq').hasClass(symbol)){
+			return true;
+		} else if ($('.sq7').hasClass(symbol) && $('.sq8').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+			return true;
+		}else if($('.sq1').hasClass(symbol) && $('.sq4').hasClass(symbol) && $('.sq7').hasClass(symbol)) {
+			return true;
+		} else if ($('.sq2').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq8').hasClass(symbol)){
+			return true;
+		} else if ($('.sq3').hasClass(symbol) && $('.sq6').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+			return true;
+		} else if ($('.sq1').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+			return true;
+		} else if ($('.sq3').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq7').hasClass(symbol)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	
 });
-
-// 	function checkIfPlayerWon(symbol){
-// 		// x
-// 		// o
-
-// 		if($('.sq1').hasClass(symbol) && $('.sq2').hasClass(symbol) && $('.sq3').hasClass(symbol)) {
-// 			return true;
-// 		} else if ($('.sq4').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq6').hasClass(symbol)){
-// 			return true;
-// 		} else if ($('.sq7').hasClass(symbol) && $('.sq8').hasClass(symbol) && $('.sq9').hasClass(symbol)){
-// 			return true;
-// 		}else if($('.sq1').hasClass(symbol) && $('.sq4').hasClass(symbol) && $('.sq7').hasClass(symbol)) {
-// 			return true;
-// 		} else if ($('.sq2').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq8').hasClass(symbol)){
-// 			return true;
-// 		} else if ($('.sq3').hasClass(symbol) && $('.sq6').hasClass(symbol) && $('.sq9').hasClass(symbol)){
-// 			return true;
-// 		} else if ($('.sq1').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq9').hasClass(symbol)){
-// 			return true;
-// 		} else if ($('.sq3').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq7').hasClass(symbol)){
-// 			return true;
-// 		} else {
-// 			return false;
-// 		}
-// 	}
-
-// });
 
 
 
