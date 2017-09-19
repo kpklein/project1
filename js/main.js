b0c25d584ce02f6a138b02da4902606384239699
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2017-09-19 15:54:48 +0100

    added new function for  game start

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ $(document).ready(function(){
 
 			
 		}else {
-			boxSelected.addClass('blue');
-			if(checkIfPlayerWon('blue')){
+			boxSelected.addClass('yellow');
+			if(checkIfPlayerWon('yellow')){
 				alert('Conrgats! Player ' + player + 'has won!')
 			}else{
 				player = 1;
@@ -40,11 +40,17 @@ $(document).ready(function(){
 	var winCounter = 0;
 	//winning combinations array 
 	var winningPossibilities = [[0,1,2,3],[1,2,3,4],[5,6,7,8],[6,7,8,9],[10,11,12,13],[11,12,13,14],[15,16,17,18],[16,17,18,19],[20,21,22,23],[21,22,23,24],[0,5,10,15],[5,10,15,20],[1,6,11,16],[6,11,16,21],[2,7,12,17],[7,12,17,22],[3,8,13,18],[8,13,18,23],[4,9,14,19],[9,14,19,24],[5,11,17,23],[0,6,12,18],[6,12,18,24],[1,7,13,19],[15,11,7,3],[21,17,13,9],[20,16,12,8],[16,12,8,4]]
-	//arrays to track the movesof players
+	//arrays to track the moves of players
 	var redMoves = [];
-	var blueMoves = [];
+	var yellowMoves = [];
 	
-
+	// function gameStartSwitch() {
+	// 	$('#startButton').click(function () {
+	// 		audio.play();
+	//     $('#startScreen').hide();
+	//     $('#gameScreen').show();
+	// 	});
+	// }
 
 
 	
